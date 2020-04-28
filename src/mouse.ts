1b11b2b29782fbc4ccd3276e90714c03e749c806
Brandon Tsang <brandononline2@gmail.com>
2020-04-27 22:50:03 -0400

    Refined view panning.

--- src/mouse.ts
+++ src/mouse.ts
@@ -15,13 +15,34 @@ function rotCam(dx: number, dy: number) { // Rotates camera without going past -
 export function mouse() {
     let panVelocities: Array<{ vec: Vector2, time: number }> = [];
     let panSpeed = 1300;
+    // a and b are parameters which have been tweaked precisely! Do not touch!
+    let a = 20;
+    let b = 1.05;
     window.addEventListener('mousemove', (evt) => {
         if (evt.buttons === 1) {
-            rotCam.call(this, evt.movementY / panSpeed / this.camera.zoom, evt.movementX / panSpeed / this.camera.zoom);
+            let pos = [
+                2 * evt.clientX / window.innerWidth - 1,
+                -(2 * evt.clientY / window.innerHeight - 1)
+            ];
+            let ray = new Ray();
+            let vec = ray.direction.set(pos[0], pos[1], -1).unproject(this.camera).normalize();
+
+            /* Without adj, moving the mouse horizontally (changing the yaw of the camera) would be too slow when zoomed
+             * in to the poles (try setting adj to 1 to see what this looked like). To compensate for this, I divide by
+             * adj1. However, this overcompensates when not zoomed in. To compensate it back, I use the maximum of adj1
+             * and adj2. Don't worry about the math. */
+            let adj1 = Math.sqrt(vec.x ** 2 + vec.z ** 2);
+            let adj2 = (((40 - this.camera.zoom) / 39) ** a - 1 + b) / b;
+            let adj = Math.max(adj1, adj2);
+            rotCam.call(
+                this,
+                evt.movementY / panSpeed / this.camera.zoom,
+                evt.movementX / panSpeed / this.camera.zoom / adj
+            );
             panVelocities.push({
                 vec: new Vector2(
                     evt.movementY / panSpeed / this.camera.zoom,
-                    evt.movementX / panSpeed / this.camera.zoom
+                    evt.movementX / panSpeed / this.camera.zoom / adj
                 ),
                 time: Date.now()
             });
